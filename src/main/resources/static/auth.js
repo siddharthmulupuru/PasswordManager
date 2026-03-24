bbb2c7d5cd726ace01b215123769a6c772d05c6b
siddharthmulupuru <siddharth100@hotmail.com>
2026-03-24 10:49:02 -0700

    Testing login api function.

---
+++ src/main/resources/static/auth.js
@@ -0,0 +1,14 @@
+// Authenticate the username and password by communicating with the API.
+// Returns the output from the authentication (JWT).
+async function authenticate(url, username, password) {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({username: username, password: password})
+    });
+
+    const data = await response.json();
+    return data;
+}
