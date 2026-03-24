bf19752f05e517c9ab4877c27cfc4903a3221c5e
siddharthmulupuru <siddharth100@hotmail.com>
2026-03-24 13:39:38 -0700

    Add login page and login functionality.

--- src/main/resources/static/auth.js
+++ src/main/resources/static/auth.js
@@ -1,5 +1,6 @@
 // Authenticate the username and password by communicating with the API.
 // Returns the output from the authentication (JWT).
+// Returns null if error occurs.
 async function authenticate(url, username, password) {
     const response = await fetch(url, {
         method: "POST",
@@ -8,6 +9,10 @@ async function authenticate(url, username, password) {
         },
         body: JSON.stringify({username: username, password: password})
     });
+
+    if (response.status !== 200) {
+        return null;
+    }
 
     const data = await response.text();
     return data;
