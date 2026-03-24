9401b00501d9b7252837cbe753835b9a0226d030
siddharthmulupuru <siddharth100@hotmail.com>
2026-03-24 11:20:19 -0700

    Update SecurityConfig public pages.

--- src/main/java/com/siddharthmulupuru/passwordmanager/config/SecurityConfig.java
+++ src/main/java/com/siddharthmulupuru/passwordmanager/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/auth/**", "/", "/*.html", "/*.js").permitAll()
                 .anyRequest().authenticated()
             );
         
